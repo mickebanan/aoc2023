005403d4eb2b067a8938cba2386c88fc483176f4
Mikael <alochmar@gmail.com>
2023-12-06 17:14:05 +0200

    Refactor.

--- 6.py
+++ 6.py
@@ -14,29 +14,31 @@ times = data[0].split()[1:]
 distances = data[1].split()[1:]
 
 
+def search(time, distance, right=False):
+    # Binary search from the left (or from the right if right=True)
+    lower, upper = 0, time
+    while lower < upper:
+        mid = (lower + upper) // 2
+        value = (time - mid) * mid
+        if right:
+            if value <= distance:
+                upper = mid
+            else:
+                lower = mid + 1
+        else:
+            if value <= distance:
+                lower = mid + 1
+            else:
+                upper = mid
+    return lower
+
+
 def get_wins(time, distance):
     # The computed distances describe a bell curve, so it's enough to just find the winning start and end points.
     # Everything in between is automatically going to win too.
     # starting point
-    lower, upper = 0, time
-    while lower < upper:
-        mid = (lower + upper) // 2
-        value = (time - mid) * mid
-        if value <= distance:
-            lower = mid + 1
-        else:
-            upper = mid
-    start = lower
-    # ending point
-    lower, upper = 0, time
-    while lower < upper:
-        mid = (lower + upper) // 2
-        value = (time - mid) * mid
-        if value <= distance:
-            upper = mid
-        else:
-            lower = mid + 1
-    end = lower
+    start = search(time, distance)
+    end = search(time, distance, right=True)
     return end - start
 
 
