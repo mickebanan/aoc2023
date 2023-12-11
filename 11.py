c2702631c764f05aecf311bcf77dea39418ea600
Micke <mikael.molsa@proton.me>
2023-12-11 10:39:32 +0200

    No need for graph algorithms here.

--- 11.py
+++ 11.py
@@ -40,77 +40,43 @@ data = transpose(data)
 empty_xs = expand(data)
 data = transpose(data)
 galaxies = set()
-ymax = len(data)
-xmax = len(data[0])
 for y, row in enumerate(data):
     for x, col in enumerate(row):
         if col == '#':
             galaxies.add((y, x))
 
 
-def get_neighbors(y, x):
-    if x:
-        yield y, x - 1
-    if x < xmax:
-        yield y, x + 1
-    if y:
-        yield y - 1, x
-    if y < ymax:
-        yield y + 1, x
-
-
-def walk(pos, part_1=True):
-    if part_1:
-        empty_space = 1
-    else:
-        empty_space = 999_999
-    distances = {}
-    pq = queue.PriorityQueue()
-    unvisited = set()
-    for y in range(ymax + 1):
-        for x in range(xmax + 1):
-            n = (y, x)
-            distances[n] = 1e8
-            unvisited.add(n)
-    distances[pos] = 0
-    pq.put((0, pos))
-    while unvisited:
-        u = pq.get()[1]
-        unvisited.remove(u)
-        for yy, xx in get_neighbors(*u):
-            if (yy, xx) in unvisited:
-                diff = 1
-                if yy in empty_ys:
-                    diff += empty_space
-                if xx in empty_xs:
-                    diff += empty_space
-                alt = distances[u] + diff
-                if alt < distances[(yy, xx)]:
-                    distances[(yy, xx)] = alt
-                    pq.put((alt, (yy, xx)))
-    return distances
+def walk(galaxy, part_1=True):
+    def get_distance(y1, x1, y2, x2):
+        ydiff = xdiff = 0
+        if part_1:
+            empty_space = 1
+        else:
+            empty_space = 999_999
+        for ey in empty_ys:
+            if y1 < ey < y2 or y2 < ey < y1:
+                ydiff += empty_space
+        for ex in empty_xs:
+            if x1 < ex < x2 or x2 < ex < x1:
+                xdiff += empty_space
+        return abs(y2 - y1) + abs(x2 - x1) + ydiff + xdiff
+    return sum(get_distance(*galaxy, *g2) for g2 in galaxies if g2 != galaxy)
 
 
 def p1():
     shortest = 0
     for galaxy in galaxies:
         d = walk(galaxy)
-        for g in galaxies:
-            if g == galaxy:
-                continue
-            shortest += d[g]
-    print('part 1:', int(shortest / 2))
+        shortest += d
+    print('part 1:', shortest // 2)
 
 
 def p2():
     shortest = 0
     for galaxy in galaxies:
         d = walk(galaxy, part_1=False)
-        for g in galaxies:
-            if g == galaxy:
-                continue
-            shortest += d[g]
-    print('part 2:', int(shortest / 2))
+        shortest += d
+    print('part 2:', shortest // 2)
 
 
 p1()
