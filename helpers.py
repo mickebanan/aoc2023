142d48bdde0afce6a339acf6a8a075378c9d3b7a
Micke <mikael.molsa@proton.me>
2023-12-14 17:06:07 +0200

    Add day 14/Parabolic Reflector Dish.
    Add some helper functions too.

--- helpers.py
+++ helpers.py
@@ -8,3 +8,26 @@ def timer(f):
         print(f'Function {f.__name__} executed in {(time.time() - t):.3f} s')
         return r
     return wrapper
+
+
+def transpose(m, as_list=False):
+    _m = []
+    for i in range(len(m[0])):
+        r = []
+        for row in m:
+            r.append(row[i])
+        if as_list:
+            _m.append(r)
+        else:
+            _m.append(''.join(r))
+    return _m
+
+
+def pp(m):
+    print('vvv')
+    for row in m:
+        if isinstance(row, list):
+            print(''.join(row))
+        else:
+            print(row)
+    print('^^^')
